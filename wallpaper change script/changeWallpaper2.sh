148ab6cf1e0b5017161350755fe43a9914f85605
agusz-source <agustinpanigutto@gmail.com>
2024-12-19 14:46:11 -0300

    Update changeWallpaper2.sh

--- wallpaper change script/changeWallpaper2.sh
+++ wallpaper change script/changeWallpaper2.sh
@@ -13,4 +13,6 @@ swww query || swww-daemon
 
 #change-wallpaper using swww
 swww img ${DIR}/${RANDOMPICS} --transition-fps 30 --transition-type any --transition-duration 3
+
+#change colors using pywal16
 wal -n -i ${DIR}/${RANDOMPICS}
