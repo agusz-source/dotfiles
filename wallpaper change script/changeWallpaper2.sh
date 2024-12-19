0d33f96f9288b8899cebd6594a5ba10a4c2bfffe
agusz-source <agustinpanigutto@gmail.com>
2024-12-18 21:51:59 -0300

    Create changeWallpaper2.sh

---
+++ wallpaper change script/changeWallpaper2.sh
@@ -0,0 +1,16 @@
+
+
+DIR=$HOME/Imágenes/wallpaper/
+PICS=($(ls ${DIR}))
+
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+if [[ $(pidof swaybg) ]]; then
+  pkill swaybg
+fi
+
+swww query || swww-daemon
+
+#change-wallpaper using swww
+swww img ${DIR}/${RANDOMPICS} --transition-fps 30 --transition-type any --transition-duration 3
+wal -n -i ${DIR}/${RANDOMPICS}
